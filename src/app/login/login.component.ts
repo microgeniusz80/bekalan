1d46cfff22d1735b4469647b145ebde6fa09f3f0
microgeniusz80 <79121880+microgeniusz80@users.noreply.github.com>
2024-10-23 22:43:05 +0800

    test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,6 +22,34 @@ export class LoginComponent implements OnInit {
     if(user == 'wd1' && password == '123'){
       environment.ward = 'Ward 1'
       this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd2' && password == '123'){
+      environment.ward = 'Ward 2'
+      this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd3' && password == '123'){
+      environment.ward = 'Ward 3'
+      this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd4' && password == '123'){
+      environment.ward = 'Ward 4'
+      this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd5' && password == '123'){
+      environment.ward = 'Ward 5'
+      this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd6' && password == '123'){
+      environment.ward = 'Ward 6'
+      this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd7' && password == '123'){
+      environment.ward = 'Ward 7'
+      this.router.navigate(['/kewpa']);
+    } else 
+    if(user == 'wd8' && password == '123'){
+      environment.ward = 'Ward 8'
+      this.router.navigate(['/kewpa']);
     } else {
       alert('access denied')
     }

20895079c7b05d857f9d6cccdb5f12dc4a6e111d
microgeniusz80 <79121880+microgeniusz80@users.noreply.github.com>
2024-12-26 21:41:11 +0800

    done

--- src/app/kewpa/kewpa.component.ts
+++ src/app/kewpa/kewpa.component.ts
@@ -74,50 +74,62 @@ export class KewpaComponent implements OnInit {
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
       {
         "data":"",
         "value":0,
+        "baki":0,
       },
     ]
   }
@@ -152,15 +164,35 @@ export class KewpaComponent implements OnInit {
   this.entry.data[11].value = this.qn12
   console.log('final data: ', this.entry)
 
+  this.entry.data[0].baki = this.bn1
+  this.entry.data[1].baki = this.bn2
+  this.entry.data[2].baki = this.bn3
+  this.entry.data[3].baki = this.bn4
+  this.entry.data[4].baki = this.bn5
+  this.entry.data[5].baki = this.bn6
+  this.entry.data[6].baki = this.bn7
+  this.entry.data[7].baki = this.bn8
+  this.entry.data[8].baki = this.bn9
+  this.entry.data[9].baki = this.bn10
+  this.entry.data[10].baki = this.bn11
+  this.entry.data[11].baki = this.bn12
+  console.log('final data with baki: ', this.entry)
+
   var checking = false
 
+  //function below check whether data is complete for each row
   this.entry.data.forEach((item, index) => {
-    if (item.data !== '' && item.value == null) {
+    if (item.data !== '' && item.value == null && item.baki == null) {
       checking = true
       console.log('sangkut: ', index)
     }
 
     if (item.value !== null && item.data == '') {
+      checking = true
+      console.log('reverse sangkut: ', index)
+    }
+
+    if (item.baki !== null && item.data == '') {
       checking = true
       console.log('reverse sangkut: ', index)
     }
@@ -705,7 +737,7 @@ export class KewpaComponent implements OnInit {
       console.log('why no response')
 
       let my_object = {}; 
-      let colum_criteria = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56];
+      let colum_criteria = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 38, 41, 44, 47, 50, 53, 56, 59, 62, 65, 68, 71, 74, 77, 80, 83];
       let description = ['BIOHAZ BAG','GEL TUBE','EDTA TUBE','GLUC TUBE','COAG TUBE','URINE CONT','PAEDS GEL', 'PAEDS FBC', 'PAEDS EDTA', 'STOOL CONT', 'SWAB - AMIES (GEL)', 'SWAB - CARRY BLAIR', 'ESR TUBE', '24 HRS URINE CONTAINER (BOX)', 'C&S AEROBIC', 'C&S ANAEROBIC', 'C&S PAEDS', 'C&S FUNGAL', 'LITHIUM HEP TUBE', 'G6PD PAPER', 'BLOOD SPOT PAPER', 'FOAM BOX', 'PARAFILM', 'BIJOUE BOTTLE', 'GLASS SLIDES', 'MICROSCOPE SLIDE', 'PLAIN TUBE + RED STOPER', 'SODIUM HEP TUBE'];
       console.log('RETURNED DATA: ', returnedData[0])
       colum_criteria.forEach((data, index)=>{
@@ -718,14 +750,15 @@ export class KewpaComponent implements OnInit {
           this.currentRecord.push(
             {
               items:description[index],
-              value:returnedData[0][data]
+              value:returnedData[0][data],
+              baki:returnedData[0][data+1]
             }
           )
         }
 
 
         this.entryStatus = ''
-        console.log('the array contains: ', this.currentRecord)
+        
         // this.currentRecord.foreach((item: any)=>{
         //   console.log('dalam current record buang kosong',item)
         // })
@@ -735,17 +768,20 @@ export class KewpaComponent implements OnInit {
         this.checkServer = true
         this.hideTable = false;
       })
-console.log('before masuk')
+
+      console.log('the array contains: ', this.currentRecord)
+      console.log('before masuk')
+
       this.currentRecord.forEach((item: any)=>{
         console.log('selepas masuk')
         if (item.value !== '0'){
           this.canvasShow.push(
             {
               items:item.items,
-              value:item.value
+              value:item.value,
+              baki:item.baki
             }
           )
-          console.log('barang:',item)
         }
       });
 
@@ -797,7 +833,7 @@ console.log('before masuk')
         ctx.fillText(this.canvasShow[i].items,220,398+i*25);
         ctx.textAlign = "center";
         ctx.fillText(this.canvasShow[i].value,490,398+i*25);
-        console.log('content: ' + this.canvasShow[i].items)
+        ctx.fillText(this.canvasShow[i].baki,700,398+i*25);
       }
 
       
@@ -836,120 +872,177 @@ console.log('before masuk')
     var kod26=0
     var kod27=0
     var kod28=0
+
+    var bkod1=0
+    var bkod2=0
+    var bkod3=0
+    var bkod4=0
+    var bkod5=0
+    var bkod6=0
+    var bkod7=0
+    var bkod8=0
+    var bkod9=0
+    var bkod10=0
+    var bkod11=0
+    var bkod12=0
+    var bkod13=0
+    var bkod14=0
+    var bkod15=0
+    var bkod16=0
+    var bkod17=0
+    var bkod18=0
+    var bkod19=0
+    var bkod20=0
+    var bkod21=0
+    var bkod22=0
+    var bkod23=0
+    var bkod24=0
+    var bkod25=0
+    var bkod26=0
+    var bkod27=0
+    var bkod28=0
     
     data.forEach(item =>{
       if (item.data !== ''){
         switch(item.data){
           case 'Biohazard_bag': {
             kod1 = item.value
+            bkod1 = item.baki
             break
           }
           case 'Gel_Tube': {
             kod2 = item.value
+            bkod2 = item.baki
             break
           }
           case 'EDTA_Tube': {
             kod3 = item.value
+            bkod3 = item.baki
             break
           }
           case 'Gluc_Tube': {
             kod4 = item.value
+            bkod4 = item.baki
             break
           }
           case 'Coag_Tube': {
             kod5 = item.value
+            bkod5 = item.baki
             break
           }
           case 'urine_container': {
             kod6 = item.value
+            bkod6 = item.baki
             break
           }
           case 'Paeds_Gel': {
             kod7 = item.value
+            bkod7 = item.baki
             break
           }
           case 'Paeds_FBC': {
             kod8 = item.value
+            bkod8 = item.baki
             break
           }
           case 'Paeds_EDTA': {
             kod9 = item.value
+            bkod9 = item.baki
             break
           }
           case 'Stool_Container': {
             kod10 = item.value
+            bkod10 = item.baki
             break
           }
           case 'Swab_Amies_gel': {
             kod11 = item.value
+            bkod11 = item.baki
             break
           }
           case 'Swab_Carry_Blair': {
             kod12 = item.value
+            bkod12 = item.baki
             break
           }
           case 'ESR_Tube': {
             kod13 = item.value
+            bkod13 = item.baki
             break
           }
           case '24h_urine_container_(BOX)': {
             kod14 = item.value
+            bkod14 = item.baki
             break
           }
           case 'C&S_Aerobic': {
             kod15 = item.value
+            bkod15 = item.baki
             break
           }
           case 'C&S_Anaerobic': {
             kod16 = item.value
+            bkod16 = item.baki
             break
           }
           case 'C&S_Peads': {
             kod17 = item.value
+            bkod17 = item.baki
             break
           }
           case 'C&S_Fungal': {
             kod18 = item.value
+            bkod18 = item.baki
             break
           }
           case 'Lithium_Heparin_Tube': {
             kod19 = item.value
+            bkod19 = item.baki
             break
           }
           case 'G6PD_Paper': {
             kod20 = item.value
+            bkod20 = item.baki
             break
           }
           case 'Blood_Spot_Paper': {
             kod21 = item.value
+            bkod21 = item.baki
             break
           }
           case 'Foam_Box': {
             kod22 = item.value
+            bkod22 = item.baki
             break
           }
           case 'Parafilm': {
             kod23 = item.value
+            bkod23 = item.baki
             break
           }
           case 'Bijoue_bottle': {
             kod24 = item.value
+            bkod24 = item.baki
             break
           }
           case 'Glass_slides': {
             kod25 = item.value
+            bkod25 = item.baki
             break
           }
           case 'Microscope_Slide': {
             kod26 = item.value
+            bkod26 = item.baki
             break
           }
           case 'Plain_tube_with_red_stopper': {
             kod27 = item.value
+            bkod27 = item.baki
             break
           }
           case 'Sodium_Heparin_tube': {
             kod28 = item.value
+            bkod28 = item.baki
             break
           }
         }
@@ -993,6 +1086,34 @@ console.log('before masuk')
             num26:kod26,
             num27:kod27,
             num28:kod28,
+            bnum1:bkod1, 
+            bnum2:bkod2,
+            bnum3:bkod3,
+            bnum4:bkod4,
+            bnum5:bkod5,
+            bnum6:bkod6,
+            bnum7:bkod7,
+            bnum8:bkod8,
+            bnum9:bkod9,
+            bnum10:bkod10,
+            bnum11:bkod11,
+            bnum12:bkod12,
+            bnum13:bkod13,
+            bnum14:bkod14,
+            bnum15:bkod15,
+            bnum16:bkod16,
+            bnum17:bkod17,
+            bnum18:bkod18,
+            bnum19:bkod19,
+            bnum20:bkod20,
+            bnum21:bkod21,
+            bnum22:bkod22,
+            bnum23:bkod23,
+            bnum24:bkod24,
+            bnum25:bkod25,
+            bnum26:bkod26,
+            bnum27:bkod27,
+            bnum28:bkod28,
             ward:this.currentClient
           }
         )

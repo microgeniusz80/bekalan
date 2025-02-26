b952baa2a49a02c47ca0402caadaae75b427c23a
microgeniusz80 <ilyas.ukm@gmail.com>
2025-02-26 22:33:59 +0800

    updated password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,131 +20,131 @@ export class LoginComponent implements OnInit {
   }
 
   verify(user:string, password:string){
-    if(user == 'upd' && password == '123'){
+    if(user == 'upd' && password == 'upd'){
       environment.ward = 'UPD'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'ed' && password == '123'){
+    if(user == 'ed' && password == 'ed'){
       environment.ward = 'ED'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd1' && password == '123'){
+    if(user == 'wd1' && password == 'wd1'){
       environment.ward = 'Ward 1'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd2' && password == '123'){
+    if(user == 'wd2' && password == 'wd2'){
       environment.ward = 'Ward 2'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd3' && password == '123'){
+    if(user == 'wd3' && password == 'wd3'){
       environment.ward = 'Ward 3'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd4' && password == '123'){
+    if(user == 'wd4' && password == 'wd4'){
       environment.ward = 'Ward 4'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd5' && password == '123'){
+    if(user == 'wd5' && password == 'wd5'){
       environment.ward = 'Ward 5'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd6' && password == '123'){
+    if(user == 'wd6' && password == 'wd6'){
       environment.ward = 'Ward 6'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd7' && password == '123'){
+    if(user == 'wd7' && password == 'wd7'){
       environment.ward = 'Ward 7'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd8' && password == '123'){
+    if(user == 'wd8' && password == 'wd8'){
       environment.ward = 'Ward 8'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'cap' && password == '123'){
+    if(user == 'cap' && password == 'cap'){
       environment.ward = 'Cap'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'wd9' && password == '123'){
+    if(user == 'wd9' && password == 'wd9'){
       environment.ward = 'Ward 9'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'scn' && password == '123'){
+    if(user == 'scn' && password == 'scn'){
       environment.ward = 'SCN'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'nicu' && password == '123'){
+    if(user == 'nicu' && password == 'nicu'){
       environment.ward = 'NICU'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'icu' && password == '123'){
+    if(user == 'icu' && password == 'icu'){
       environment.ward = 'ICU'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'lr' && password == '123'){
+    if(user == 'lr' && password == 'lr'){
       environment.ward = 'LR'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'ot' && password == '123'){
+    if(user == 'ot' && password == 'ot'){
       environment.ward = 'OT'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'opd' && password == '123'){
+    if(user == 'opd' && password == 'opd'){
       environment.ward = 'OPD'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'mopd' && password == '123'){
+    if(user == 'mopd' && password == 'mopd'){
       environment.ward = 'MOPD'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'og' && password == '123'){
+    if(user == 'og' && password == 'og'){
       environment.ward = 'OG'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'popd' && password == '123'){
+    if(user == 'popd' && password == 'popd'){
       environment.ward = 'POPD'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'sopd' && password == '123'){
+    if(user == 'sopd' && password == 'sopd'){
       environment.ward = 'SOPD'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'ortho' && password == '123'){
+    if(user == 'ortho' && password == 'ortho'){
       environment.ward = 'ORTHO'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'forensik' && password == '123'){
+    if(user == 'forensik' && password == 'forensik'){
       environment.ward = 'FORENSIK'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'hdu' && password == '123'){
+    if(user == 'hdu' && password == 'hdu'){
       environment.ward = 'HDU'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'hduremis' && password == '123'){
+    if(user == 'hduremis' && password == 'hduremis'){
       environment.ward = 'HDUREMIS'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'eye' && password == '123'){
+    if(user == 'eye' && password == 'eye'){
       environment.ward = 'EYE'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'psy' && password == '123'){
+    if(user == 'psy' && password == 'psy'){
       environment.ward = 'PSY'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'xray' && password == '123'){
+    if(user == 'xray' && password == 'xray'){
       environment.ward = 'XRAY'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'cssu' && password == '123'){
+    if(user == 'cssu' && password == 'cssu'){
       environment.ward = 'CSSU'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'ukkp' && password == '123'){
+    if(user == 'ukkp' && password == 'ukkp'){
       environment.ward = 'UKKP'
       this.router.navigate(['/kewpa']);
     } else 
-    if(user == 'pathobb' && password == '123'){
+    if(user == 'pathobb' && password == 'pathobb'){
       environment.ward = 'PATOBB'
       this.router.navigate(['/kewpa']);
     } else 
